474810ead337d732d11cfb491d61c6707b37133b
hallaman <heather.allaman@gmail.com>
2016-03-08 12:28:10 -0600

    application.yml

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,8 +72,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :user_name            => 'heather.allaman@gmail.com',
-   :password             => 'DollyBaby1984',
+   :user_name            => ENV['username'],
+   :password             => ENV['password'],
    :authentication       => "plain",
    :enable_starttls_auto => true
   }
