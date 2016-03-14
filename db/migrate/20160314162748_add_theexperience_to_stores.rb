3746b85664338ccf7c9b00b9930a610f4d00e9fd
hallaman <heather.allaman@gmail.com>
2016-03-14 13:37:33 -0500

    adding experience field to form

--- db/migrate/20160314162748_add_theexperience_to_stores.rb
+++
@@ -1,5 +0,0 @@
-class AddTheexperienceToStores < ActiveRecord::Migration
-  def change
-    add_column :stores, :theexperience, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160314162748) do
+ActiveRecord::Schema.define(version: 20160314170045) do
 
   create_table "stores", force: :cascade do |t|
     t.string   "name"
