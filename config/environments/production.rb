3514e39e0e8b023fdd019fd7925ffbf6ed5c9ab9
treasurecat <treasurecat@gmail.com>
2016-04-01 17:56:01 -0500

    set config.serve_static_files and config.assets.compile to true to display images correctly

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,12 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # In production mode (on heroku) rails compiles images, etc., to public/assets.  These
+  # precompiled images are served, not the files in assets
+  config.serve_static_files = true
+  # fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = true
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -22,14 +28,14 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # config.assets.compile = false
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
@@ -63,7 +69,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  
+
   config.action_mailer.delivery_method = :smtp
 
   # SMTP settings for gmail
