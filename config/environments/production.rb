b3f3760bfe9377bdbe86372daa9cb374a7b1002c
hallaman <heather.allaman@gmail.com>
2016-04-30 15:05:03 -0500

    default from noreply

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,7 @@ Rails.application.configure do
    :authentication       => "plain",
    :enable_starttls_auto => true
   }
+  config.action_mailer.default_options = {from: 'no-reply@willowbride.com'}
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
