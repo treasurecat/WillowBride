45f3159682e0bfaca2e1abd9341b912898425e0d
hallaman <heather.allaman@gmail.com>
2017-05-10 10:35:51 -0500

    aws region

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,10 +100,12 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     :storage => :s3,
+    :s3_host_name => 's3-us-east-1.amazonaws.com',
     :s3_credentials => {
       :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_KEY_ID'],
-      :secret_access_key => ENV['AWS_ACCESS_KEY']
+      :secret_access_key => ENV['AWS_ACCESS_KEY'],
+      :s3_region => 'us-east-1'
     }
   }
 
